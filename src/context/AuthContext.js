1dcb574898914b1735089195b6e471369085a968
4FRO <drotich@ucsc.edu>
2023-01-03 19:05:25 -0800

    ResolveAuth and Signout complete

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -11,8 +11,20 @@ const authReducer = (state, action) => {
       return { errorMessage: '', token: action.payload };
     case 'clear_error_message':
       return { ...state, errorMessage: '' };
+    case 'signout':
+      return { token: null, errorMessage: '' };
     default:
       return state;
+  }
+};
+
+const tryLocalSignin = (dispatch) => async () => {
+  const token = await AsyncStorage.getItem('token');
+  if (token) {
+    dispatch({ type: 'signin', payload: token });
+    navigate('TrackList');
+  } else {
+    navigate('loginFlow');
   }
 };
 
@@ -58,14 +70,14 @@ const signin =
     }
   };
 
-const signout = (dispatch) => {
-  return () => {
-    //handle signout
-  };
+const signout = (dispatch) => async () => {
+  await AsyncStorage.removeItem('token');
+  dispatch({ type: 'signout' });
+  navigate('loginFlow');
 };
 
 export const { Provider, Context } = createDataContext(
   authReducer,
-  { clearErrorMessage, signin, signout, signup },
+  { clearErrorMessage, signin, signout, signup, tryLocalSignin },
   { token: null, errorMessage: '' }
 );
